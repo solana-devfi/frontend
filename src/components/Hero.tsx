da5255343d95b66fb9e6b80e055c619b6d5a9021
Marcus Pang <marcuspangyuyang@gmail.com>
2023-03-08 21:24:12 +0800

    feat: add dark mode for button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,19 +24,7 @@ export function Hero() {
         opposite trade-off, and hope you don’t get audited.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="/register">Get 6 months free</Button>
-        <Button
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          variant="outline"
-        >
-          <svg
-            aria-hidden="true"
-            className="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
-          >
-            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
-          </svg>
-          <span className="ml-3">Watch video</span>
-        </Button>
+        <Button href="/organisations">Get Started</Button>
       </div>
     </Container>
   );
